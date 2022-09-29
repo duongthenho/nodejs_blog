9550b824ee331ce9916f2436dbced8464f99af3c
duongthenho <69722419+duongthenho@users.noreply.github.com>
2022-09-29 08:17:15 +0700

    new commit

--- src/index.js
+++ src/index.js
@@ -3,19 +3,24 @@ const path = require('path');
 const handlebars = require('express-handlebars');
 
 const morgan = require('morgan');
-const { dirname } = require('path');
 
 const app = express();
 const port = 3000;
 
+//Static file
 app.use(express.static(path.join(__dirname, 'public')));
+
+//middleware
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
 
 //HTTP logger
 app.use(morgan('combined'));
 
 //template engine
 app.engine('hbs', handlebars.engine({
-  extname : ".hbs"
+  extname: ".hbs"
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
@@ -26,9 +31,13 @@ app.get('/trang-chu', (req, res) => {
 
 app.get('/news', (req, res) => {
   res.render('news');
-
 });
-
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+app.post('/news', (req, res) => {
+  res.render('search');
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
