2af4693e4cb3565d49437bae8a282c109ff3dba5
duongthenho <69722419+duongthenho@users.noreply.github.com>
2022-10-25 16:03:05 +0700

    cm

--- src/index.js
+++ src/index.js
@@ -13,23 +13,24 @@ const route = require('./routes');
 app.use(express.static(path.join(__dirname, 'public')));
 
 //middleware
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 //HTTP logger
 app.use(morgan('combined'));
 
 //template engine
-app.engine('hbs', handlebars.engine({
-  extname: ".hbs"
-}));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 route(app);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 });
