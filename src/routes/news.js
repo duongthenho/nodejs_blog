3106cddd66b23f545fc3d165cd4968e6a45c7c59
duongthenho <69722419+duongthenho@users.noreply.github.com>
2022-10-25 16:18:35 +0700

    c

--- src/routes/news.js
+++ src/routes/news.js
@@ -5,6 +5,6 @@ const newsController = require('../app/controllers/NewsController');
 
 //
         router.use('/:slug', newsController.detail);
-router.use('/', newsController.index);
+        router.use('/', newsController.index);
 
 module.exports = router;
